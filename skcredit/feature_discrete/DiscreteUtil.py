d82ad4c0ae685dae50f368e16e6e45f94c7a4243
JYLFamily <1579502635@qq.com>
2019-11-14 16:04:58 +0800

    update merge_cat_table function

--- skcredit/feature_discrete/DiscreteUtil.py
+++ skcredit/feature_discrete/DiscreteUtil.py
@@ -11,49 +11,34 @@ pd.set_option("max_columns", None)
 logging.basicConfig(format="[%(asctime)s]-[%(filename)s]-[%(levelname)s]-[%(message)s]", level=logging.INFO)
 
 
-def calc_part_table(X, col, col_type):
+def calc_part_table(X, col):
     """
     :param X:
     :param col:
-    :param col_type:
     :return:
     """
-
     x = X.copy(deep=True)
     del X
     gc.collect()
 
-    if col_type == "numeric":
-        lower_bin = x.groupby(col + "_bin")[col].min().to_frame("Lower").reset_index(drop=True)
-        upper_bin = x.groupby(col + "_bin")[col].max().to_frame("Upper").reset_index(drop=True)
+    lower_bin = x.groupby(col + "_bin")[col].min().to_frame("Lower").reset_index(drop=True)
+    upper_bin = x.groupby(col + "_bin")[col].max().to_frame("Upper").reset_index(drop=True)
 
-        cnt_rec = x.groupby(col + "_bin")["target"].agg(len).to_frame("CntRec").reset_index(drop=True)
-        cnt_positive = x.loc[x["target"] == 1, [col + "_bin", "target"]].groupby(
-            col + "_bin")["target"].agg(len).to_frame("CntPositive").reset_index(drop=True)
-        cnt_negative = x.loc[x["target"] == 0, [col + "_bin", "target"]].groupby(
-            col + "_bin")["target"].agg(len).to_frame("CntNegative").reset_index(drop=True)
+    cnt_rec = x.groupby(col + "_bin")["target"].agg(len).to_frame("CntRec").reset_index(drop=True)
+    cnt_positive = x.loc[x["target"] == 1, [col + "_bin", "target"]].groupby(
+        col + "_bin")["target"].agg(len).to_frame("CntPositive").reset_index(drop=True)
+    cnt_negative = x.loc[x["target"] == 0, [col + "_bin", "target"]].groupby(
+        col + "_bin")["target"].agg(len).to_frame("CntNegative").reset_index(drop=True)
 
-        part_table = pd.concat([lower_bin, upper_bin, cnt_rec, cnt_positive, cnt_negative], axis=1)
-    else:
-        cnt_rec = x.groupby(col)["target"].agg(len).to_frame("CntRec")
-        cnt_positive = x.loc[x["target"] == 1, [col, "target"]].groupby(
-            col)["target"].agg(len).to_frame("CntPositive")
-        cnt_negative = x.loc[x["target"] == 0, [col, "target"]].groupby(
-            col)["target"].agg(len).to_frame("CntNegative")
-
-        cnt_positive = cnt_positive.reindex(index=cnt_rec.index)
-        cnt_negative = cnt_negative.reindex(index=cnt_rec.index)
-
-        part_table = pd.concat([cnt_rec, cnt_positive, cnt_negative], axis=1)
+    part_table = pd.concat([lower_bin, upper_bin, cnt_rec, cnt_positive, cnt_negative], axis=1)
 
     return part_table
 
 
-def calc_table(X, col, col_type):
+def calc_table(X, col):
     """
     :param X:
     :param col:
-    :param col_type:
     :return:
     """
     x = X.copy(deep=True)
@@ -63,41 +48,11 @@ def calc_table(X, col, col_type):
     x_non = x.loc[x[col] != -9999, :].copy(deep=True)
     x_mis = x.loc[x[col] == -9999, :].copy(deep=True)
 
-    num_columns = [
+    columns = [
         "Lower", "Upper", "CntRec", "CntPositive", "CntNegative", "PositiveRate", "NegativeRate", "WoE", "IV"]
-    cat_columns = [
-        "CntRec", "CntPositive", "CntNegative", "PositiveRate", "NegativeRate", "WoE", "IV"]
 
-    if col_type == "numeric":
-        if x_mis.empty is True:
-            table = calc_part_table(x_non, col, col_type)
-
-            table["PositiveRate"] = table["CntPositive"] / table["CntPositive"].sum()
-            table["NegativeRate"] = table["CntNegative"] / table["CntNegative"].sum()
-
-            table["WoE"] = np.log(table["PositiveRate"] / table["NegativeRate"])
-            table["IV"] = (table["PositiveRate"] - table["NegativeRate"]) * table["WoE"]
-
-            table = table.reindex(columns=num_columns).reset_index(drop=True)
-            table.loc[0, "Lower"], table.loc[table.shape[0] - 1, "Upper"] = -np.inf, np.inf
-        else:
-            non_table = calc_part_table(x_non, col, col_type)
-            mis_table = calc_part_table(x_mis, col, col_type)
-            table = pd.concat([non_table.reindex(columns=num_columns), mis_table.reindex(columns=num_columns)])
-
-            table["PositiveRate"] = table["CntPositive"] / table["CntPositive"].sum()
-            table["NegativeRate"] = table["CntNegative"] / table["CntNegative"].sum()
-
-            table["WoE"] = np.log(table["PositiveRate"] / table["NegativeRate"])
-            table["IV"] = (table["PositiveRate"] - table["NegativeRate"]) * table["WoE"]
-
-            table = table.reindex(columns=num_columns).reset_index(drop=True)
-            table.loc[0, "Lower"], table.loc[table.shape[0] - 2, "Upper"] = -np.inf, np.inf
-    else:
-        table = calc_part_table(x, col, col_type)
-
-        table["CntPositive"] = table["CntPositive"].fillna(0.5)
-        table["CntNegative"] = table["CntNegative"].fillna(0.5)
+    if x_mis.empty is True:
+        table = calc_part_table(x_non, col)
 
         table["PositiveRate"] = table["CntPositive"] / table["CntPositive"].sum()
         table["NegativeRate"] = table["CntNegative"] / table["CntNegative"].sum()
@@ -105,7 +60,21 @@ def calc_table(X, col, col_type):
         table["WoE"] = np.log(table["PositiveRate"] / table["NegativeRate"])
         table["IV"] = (table["PositiveRate"] - table["NegativeRate"]) * table["WoE"]
 
-        table = table.reindex(columns=cat_columns).reset_index(drop=False)
+        table = table.reindex(columns=columns).reset_index(drop=True)
+        table.loc[0, "Lower"], table.loc[table.shape[0] - 1, "Upper"] = -np.inf, np.inf
+    else:
+        non_table = calc_part_table(x_non, col)
+        mis_table = calc_part_table(x_mis, col)
+        table = pd.concat([non_table.reindex(columns=columns), mis_table.reindex(columns=columns)])
+
+        table["PositiveRate"] = table["CntPositive"] / table["CntPositive"].sum()
+        table["NegativeRate"] = table["CntNegative"] / table["CntNegative"].sum()
+
+        table["WoE"] = np.log(table["PositiveRate"] / table["NegativeRate"])
+        table["IV"] = (table["PositiveRate"] - table["NegativeRate"]) * table["WoE"]
+
+        table = table.reindex(columns=columns).reset_index(drop=True)
+        table.loc[0, "Lower"], table.loc[table.shape[0] - 2, "Upper"] = -np.inf, np.inf
 
     return table
 
@@ -135,7 +104,7 @@ def merge_num_table(X, col, merge_bin):
         x_mis[col + "_bin"] = -9999
         x = pd.concat([x_non, x_mis[x_non.columns]])
 
-        table = calc_table(x, col, "numeric")
+        table = calc_table(x, col)
         if (table.loc[table["Upper"] != -9999, "WoE"].is_monotonic_increasing or
                 table.loc[table["Upper"] != -9999, "WoE"].is_monotonic_decreasing):
             break
@@ -145,41 +114,53 @@ def merge_num_table(X, col, merge_bin):
     return table
 
 
-def merge_cat_table(X, col, merge_gap):
+def merge_cat_table(X, col, merge_bin):
     """
     :param X:
     :param col:
-    :param merge_gap:
+    :param merge_bin:
     :return:
     """
     x = X.copy(deep=True)
     del X
     gc.collect()
 
-    table = calc_table(x, col, "categorical")
-    gc.collect()
+    # cat to num
+    x_non = x.loc[x[col] != "missing", :].copy(deep=True)
+    x_mis = x.loc[x[col] == "missing", :].copy(deep=True)
 
-    mis_table = table.loc[table[col] == "missing", :].copy(deep=True)
-    non_table = table.loc[table[col] != "missing", :].sort_values(
-        by="WoE", ascending=True).reset_index(
-        drop=True).copy(deep=True)
+    weights = 1 / (1 + np.exp(-(x_non.groupby(col).size() - 1)))
+    mapping = (1 - weights) * x_non["target"].mean() + weights * x_non.groupby(col)["target"].mean()
+    mapping = mapping.to_dict()
 
-    merge_flag = non_table["WoE"].diff().min()
-    while merge_flag <= merge_gap:
-        idx = list(non_table["WoE"].diff()).index(merge_flag)
+    x_non[col] = x_non[col].replace(mapping)
+    x_mis[col] = x_mis[col].replace({"missing": -9999.0})
 
-        x = x.replace({non_table.loc[idx - 1, col]: (non_table.loc[idx - 1, col] + ", " + non_table.loc[idx, col])})
-        x = x.replace({non_table.loc[idx, col]: (non_table.loc[idx - 1, col] + ", " + non_table.loc[idx, col])})
+    # merge num table
+    table = merge_num_table(pd.concat([x_non, x_mis]), col, merge_bin)
 
-        table = calc_table(x, col, "categorical")
-        mis_table = table.loc[table[col] == "missing", :].copy(deep=True)
-        non_table = table.loc[table[col] != "missing", :].sort_values(
-            by="WoE", ascending=True).reset_index(
-            drop=True).copy(deep=True)
-        merge_flag = non_table["WoE"].diff().min()
-    table = pd.concat([mis_table, non_table.reindex(columns=mis_table.columns)]).reset_index(drop=True)
+    # clean num table
+    level = [[] for _ in range(len(table))]
 
-    logging.info(col + " complete !")
+    non_table = table.loc[table["Upper"] != -9999, :].copy(deep=True)
+    mis_table = table.loc[table["Upper"] == -9999, :].copy(deep=True)
+
+    for key, val in mapping.items():
+        for index, upper in enumerate(non_table["Upper"]):
+            if val <= upper:
+                level[index].append(key)
+                break
+
+    if not mis_table.empty:
+        level[-1].append("missing")
+
+    table = pd.concat([
+        pd.Series([", ".join(element) for element in level]).to_frame(col),
+        pd.concat([
+            non_table.drop(["Lower", "Upper"], axis=1),
+            mis_table.drop(["Lower", "Upper"], axis=1)
+        ])
+    ], axis=1)
 
     return table
 
