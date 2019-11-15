d45f5ffd44e608bb35c759626d69b23d36370282
JYLFamily <1579502635@qq.com>
2019-11-15 14:35:07 +0800

    add cv

---
+++ skcredit/models/util.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+np.random.seed(7)
+pd.set_option("max_rows", None)
+pd.set_option("max_columns", None)
+
+
+def cv():
+    pass
+
+    return
