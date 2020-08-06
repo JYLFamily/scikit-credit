a58710996a9535aa5306f912c26de7679ab97216
JYLFamily <1579502635@qq.com>
2020-08-06 14:05:27 +0800

    init commit

--- skcredit/feature_selection/__init__.py
+++ skcredit/feature_selection/__init__.py
@@ -2,6 +2,7 @@
 
 from skcredit.feature_selection.SelectBin import SelectBin
 from skcredit.feature_selection.SelectVif import SelectVif
+from skcredit.feature_selection.SelectViz import SelectViz
 
 __all__ = [
     "SelectBin",
