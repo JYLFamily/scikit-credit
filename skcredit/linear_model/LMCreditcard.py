7c2298103de7180b56a988dc3c969304fe4e8b88
JYLFamily <1579502635@qq.com>
2021-09-30 09:14:39 +0800

    init commit

--- skcredit/linear_model/LMCreditcard.py
+++ skcredit/linear_model/LMCreditcard.py
@@ -1,10 +1,14 @@
 # coding:utf-8
 
+import warnings
 import numpy  as np
 import pandas as pd
 np.random.seed(7)
-pd.set_option("max_rows",    None)
+pd.set_option("max_rows"   , None)
 pd.set_option("max_columns", None)
+pd.set_option("display.unicode.east_asian_width" , True)
+pd.set_option("display.unicode.ambiguous_as_wide", True)
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 class LMCreditcard(object):
