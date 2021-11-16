05e4f78d60ac391a3f649a350d19ac6eec4cda0e
JYLFamily <1579502635@qq.com>
2021-11-16 17:00:06 +0800

    init commit

--- skcredit/feature_bucketer/SplitMixND.py
+++ skcredit/feature_bucketer/SplitMixND.py
@@ -53,7 +53,7 @@ class SplitMixND(BaseSplitND):
 
         table["Bucket"] = table["Bucket"].apply(
             lambda buckets: ', '.join([
-                num_bucket_to_string(bucket) if column in self.num_columns   else
+                num_bucket_to_string(bucket) if column  in  self.num_columns else
                 cat_bucket_to_string(bucket, self.woe_encoder.lookup[column]) for
                 column, bucket in zip(self.columns, buckets)]))
 
